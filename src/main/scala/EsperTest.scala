88aa884f0a489a2bf4d3ad9b96d8b51774256a38
jeeffy <jeeffy@126.com>
2016-11-03 10:50:12 +0800

    fix bug

--- src/main/scala/EsperTest.scala
+++ src/main/scala/EsperTest.scala
@@ -1,42 +1,43 @@
 import java.util
 
 import org.apache.spark.{SparkConf, SparkContext}
-import com.espertech.esper.client.EPAdministrator;
-import com.espertech.esper.client.EPRuntime;
-import com.espertech.esper.client.EPServiceProvider;
-import com.espertech.esper.client.EPServiceProviderManager;
-import com.espertech.esper.client.EPStatement;
-import com.espertech.esper.client.EventBean;
-import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.EPAdministrator
+import com.espertech.esper.client.EPRuntime
+import com.espertech.esper.client.EPServiceProvider
+import com.espertech.esper.client.EPServiceProviderManager
+import com.espertech.esper.client.EPStatement
+import com.espertech.esper.client.EventBean
+import com.espertech.esper.client.UpdateListener
 
 object EsperTest {
+  var init = true
   def main(args: Array[String]) {
-    val file = "/Users/jeeffy/projects/scala-test/src/main/resources/data/users.txt" // Should be some file on your system
+    val file = "src/main/resources/data/users.txt"
     val conf = new SparkConf().setAppName("Simple Application")
     conf.setMaster("local[2]")
     val sc = new SparkContext(conf)
     val users = sc.textFile(file)
-    var init = true
+
     users.foreach{ line =>
-      val user = line.split(" ")
+      val user = line.trim.split(" ")
       if(line != ""){
         val map = new util.HashMap[String,Any]()
         map.put("name", user(0))
         map.put("age", user(1).toInt)
         map.put("hobby",user(2))
 
-        esperHandle(map,init)
-        init = false
+        esperHandle(map)
       }
 
     }
     sc.stop()
   }
 
-  def esperHandle(map: util.HashMap[String,Any], init: Boolean): Unit = {
+  def esperHandle(map: util.HashMap[String,Any]): Unit = {
     val epService = EPServiceProviderManager.getDefaultProvider()
     val admin = epService.getEPAdministrator()
     if(init){
+      //val userType = new util.HashMap[String,Class[_ <: Any]]()
       val userType = new util.HashMap[String,Object]()
       userType.put("name", classOf[String])
       userType.put("age", classOf[Int])
@@ -57,6 +58,7 @@ object EsperTest {
           }
         }
       })
+      init = false
     }
 
     val runtime = epService.getEPRuntime()
